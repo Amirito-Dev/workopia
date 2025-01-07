ce1f655bb650ac5ba10b6c8228a982c7520c9701
Amir <contact@amiryns.ch>
2025-01-07 22:06:04 +0100

    add initial job views and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    $title = "Available Jobs";
+    $jobs = [
+        'Web Developer',
+        'Database Admin',
+        'Software Engineer',
+        'System Analyst'
+    ];
+
+    return view('jobs.index', [
+        'title' => $title,
+        'jobs' => $jobs
+    ]);
+})->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
