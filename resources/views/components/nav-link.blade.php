72faf5653cd02df614752eb4fad6232c23ee5b79
Amir <contact@amiryns.ch>
2025-01-08 22:29:38 +0100

    fix mobile hamburger toggle and navigation state

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,12 +2,22 @@
 @props([
     'active' => false,
     'url' => '/',
-    'icon' => null
+    'icon' => null,
+    'mobile' => null
     ])
 
-<a href="{{ url($url) }}" class="text-white hover:underline py-2 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+@if($mobile)
+<a href="{{ $url }}" class="block px-4 py-2 hover:bg-blue-700 {{$active ? 'text-yellow-500 font-bold' : ''}}">
     @if($icon)
-    <i class="fa fa-{{ $icon }} mr-1"></i>
+        <i class="fa fa-{{ $icon }} mr-1"></i>
     @endif
     {{ $slot }}
-</a>
+    </a>
+@else
+    <a href="{{ $url }}" class="text-white hover:underline py-2 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+        @if($icon)
+            <i class="fa fa-{{ $icon }} mr-1"></i>
+        @endif
+        {{ $slot }}
+    </a>
+@endif
