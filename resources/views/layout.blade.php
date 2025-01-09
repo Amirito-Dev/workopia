9f56b5a95c77458a529f2d4b138c99b90ad0fa40
Amir <contact@amiryns.ch>
2025-01-09 11:07:34 +0100

    add hero, bottom banner and top banner components with css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 <body>
     <x-header />
+
+    @if(request()->is('/'))
+        <x-hero />
+        <x-top-banner />
+    @endif
+
+
     <main class="container mx-auto p-4 mt-4">
-        {{$slot}}
+        {{ $slot }}
     </main>
 
     <script src="{{ asset('js/mobile.js') }}"></script>
