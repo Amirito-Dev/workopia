ed5c2c25f53bedddec15bb2ffffbdb0b2ed78916
Amir <contact@amiryns.ch>
2025-01-10 16:44:43 +0100

    update view to add missing job details with style

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-<h1>Available Jobs</h1>
-<ul>
-    @forelse($jobs as $job)
-        <li> <a href="{{route('jobs.show', $job->id)}}">{{$loop->index}} - {{$job->title}}</a> </li>
-    @empty
-        <li>No jobs available</li>
-    @endforelse
-</ul>
+<h1 class="text-2xl">All Jobs</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+        @forelse($jobs as $job)
+            <x-job-card :job="$job" />
+        @empty
+            <li>No jobs available</li>
+        @endforelse
+    </div>
 </x-layout>
